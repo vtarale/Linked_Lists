84cef3b7856162aa1cd5c0b45615f669db3d97a8
Vihaan Tarale <taralevihaan@gmail.com>
2022-02-26 15:55:29 +0530

    still doing

--- src/linked_lists.c
+++ src/linked_lists.c
@@ -13,10 +13,14 @@ int index(struct node *, int);
 void insert(struct node **, int, int);
 void display(struct node *);
 void delete(struct node **, int);
+void _dump(struct node *);
 void dump(struct node **);
 
 int main(void){
+    struct node *linked_list;
+    linked_list = NULL;
 
+    
 }
 
 void append(struct node **root, int data){
@@ -88,4 +92,30 @@ void display(struct node *root){
         root = root->next;
     }
     printf("\n");
+}
+
+void delete(struct node **root, int pos){
+    struct node *temp, *old;
+    for(int i = 0;i < pos; ++i){
+        if(i == 0){
+            temp = *root;
+            continue;
+        }
+        old = temp;
+        temp = temp->next;
+    }
+    old->next = temp->next;
+    free(temp);
+}
+
+void _dump(struct node *root){
+    if(root != NULL){
+        _dump(root->next);
+        free(root);
+    }
+}
+
+void dump(struct node **root){
+    _dump(*root);
+    *root = NULL;
 }
